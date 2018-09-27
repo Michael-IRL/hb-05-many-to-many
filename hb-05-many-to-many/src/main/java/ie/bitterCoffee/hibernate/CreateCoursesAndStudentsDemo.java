07833729a7ca728db993a6220a73c19d83285fa4
Michael-IRL <iszekeell@gmail.com>
2018-09-27 16:52:28 +0100

    Add CreateCoursesAndStudentsDemo

---
+++ hb-05-many-to-many/src/main/java/ie/bitterCoffee/hibernate/CreateCoursesAndStudentsDemo.java
@@ -0,0 +1,76 @@
+package ie.bitterCoffee.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import ie.bitterCoffee.hibernate.entity.Course;
+import ie.bitterCoffee.hibernate.entity.Instructor;
+import ie.bitterCoffee.hibernate.entity.InstructorDetail;
+import ie.bitterCoffee.hibernate.entity.Review;
+import ie.bitterCoffee.hibernate.entity.Student;
+
+public class CreateCoursesAndStudentsDemo
+{
+
+	public static void main(String[] args)
+	{
+		//Create session factory
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetail.class)
+									.addAnnotatedClass(Course.class)
+									.addAnnotatedClass(Review.class)
+									.addAnnotatedClass(Student.class)
+									.buildSessionFactory();
+		
+		//Create session
+		Session session = factory.getCurrentSession();
+		
+		try
+		{						
+			//start a transaction
+			session.beginTransaction();
+			
+			//create a course
+			Course course = new Course("Pinball wizzard");			
+			
+			
+			//save the course
+			System.out.println("Saving the course ..");
+			session.save(course);
+			System.out.println("Course saved");
+			
+			//create students			
+			Student student1 = new Student("John","Doe","johndoe@example.com");
+			Student student2 = new Student("Mary","Public","marypublic@example.com");
+			
+			
+			//add students to course
+			course.addStudent(student1);
+			course.addStudent(student2);
+			
+			//save the students
+			System.out.println("Saving students ..");
+			session.save(student1);
+			session.save(student2);
+			System.out.println("Saved students: "+course.getStudents());
+			
+			//commit transaction
+			session.getTransaction().commit();
+			System.out.println("Done!");
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			session.close();
+			factory.close();
+		}			
+	}
+
+}
