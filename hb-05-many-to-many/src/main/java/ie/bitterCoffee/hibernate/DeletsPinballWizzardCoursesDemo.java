2494697eb0690305a048f380a04b1a2100aea519
Michael-IRL <iszekeell@gmail.com>
2018-09-27 21:27:04 +0100

    Add DeletePinballWizzardCoursesDemo

---
+++ hb-05-many-to-many/src/main/java/ie/bitterCoffee/hibernate/DeletsPinballWizzardCoursesDemo.java
@@ -0,0 +1,62 @@
+package ie.bitterCoffee.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import ie.bitterCoffee.hibernate.entity.Course;
+import ie.bitterCoffee.hibernate.entity.Instructor;
+import ie.bitterCoffee.hibernate.entity.InstructorDetail;
+import ie.bitterCoffee.hibernate.entity.Review;
+import ie.bitterCoffee.hibernate.entity.Student;
+
+public class DeletsPinballWizzardCoursesDemo
+{
+
+	public static void main(String[] args)
+	{
+		//Create session factory
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetail.class)
+									.addAnnotatedClass(Course.class)
+									.addAnnotatedClass(Review.class)
+									.addAnnotatedClass(Student.class)
+									.buildSessionFactory();
+		
+		//Create session
+		Session session = factory.getCurrentSession();
+		
+		try
+		{						
+			//start a transaction
+			session.beginTransaction();
+			
+			//get course from database
+			int courseId = 10;
+			Course course = session.get(Course.class, courseId);
+			
+			System.out.println("\nLoaded Coures: "+course);
+			System.out.println("Students: "+course.getStudents());
+			
+			//delete course
+			session.delete(course);
+			
+			//commit transaction
+			session.getTransaction().commit();
+			System.out.println("Done!");
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			session.close();
+			factory.close();
+		}			
+	}
+
+}
